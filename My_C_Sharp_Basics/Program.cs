c33ab9aa085d170d3be34afa7a1ef0f681416963
Chris Seale <sealechris@gmail.com>
2017-03-22 17:02:22 -0500

    Initial commit

---
+++ My_C_Sharp_Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_C_Sharp_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
